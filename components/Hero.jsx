030f199140e84595d9fc74981db6f8963d0f8794
Affaan Kidwai <kidwaiaffaan@gmail.com>
2022-11-30 21:55:12 +0530

    update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,11 +6,13 @@ const Hero = ({ heading, message }) => {
       {/* Overlay */}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
       <div className="p-1 text-white z-[2] mt-[-5rem]">
-        <h2 className="text-8xl font-bold">Hi, I'm Affaan Kidwai.</h2>
+        <h2 className="text-8xl font-bold">Hello! Affaan here</h2>
         <p className="py-5 text-xl">
           I capture the natural beauty of our world through my lens!!
         </p>
-        <button className=" px-8 py-2 border border-gray-400">Have a look around!</button>
+        <button className=" px-8 py-2 border border-gray-400">
+          Have a look around!
+        </button>
       </div>
     </div>
   );
